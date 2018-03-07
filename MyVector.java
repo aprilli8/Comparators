1fb68c0d4874dcc7530f8b05ae5a7414d7a25fc4
bill jannen <jannen@cs.williams.edu>
2018-03-07 11:37:34 -0500

    starter files

---
+++ MyVector.java
@@ -0,0 +1,6 @@
+import structure5.*;
+import java.util.Comparator;
+
+public class MyVector<E> extends Vector<E> {
+
+}
---
+++ Student.java
@@ -0,0 +1,8 @@
+public class Student {
+
+	// Be sure to include a toString method here.
+	public String toString() {
+		return "";
+	}
+
+}
