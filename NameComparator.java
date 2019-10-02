84f10bb66bf999a3e5019f8e29a99e3b23997095
23ayl2 <23ayl2@tcl216-12.cs.williams.edu>
2019-10-02 15:44:11 -0400

    working on namecomparator

--- NameComparator.java
+++ NameComparator.java
@@ -1,3 +1,22 @@
+import structure5.*;
+import java.util.Comparator;
+
 public class NameComparator implements Comparator<Student>{
 
+  /*
+   * pre: method parameters are valid Student objects
+   * post: returns -1 if
+   */
+  public int compare(Student a, Student b){
+    if(a.getName().compareToIgnoreCase(b.getName()) < 0){
+      return -1;
+    }
+    else if(a.getName().compareToIgnoreCase(b.getName()) == 0){
+      return 0;
+    }
+    else{
+      return 1;
+    }
+  }
+  //((String)a.getName().charAt(i)).equals((String)b.getName().charAt(i))
 }
