76b54804987abf695f3ac5b8b5d22a3804be74a3
jposner123 <jep4@williams.edu>
2019-10-06 15:39:08 -0400

    Finished AddressComparator and answered question 5 (move main method).

--- AddressComparator.java
+++ AddressComparator.java
@@ -14,11 +14,32 @@ public class AddressComparator implements Comparator<Association<String, Integer
     }
   }
 
+  //Pre: Takes a MyVector filled with students from the phonebook as an argument
+  //Post: Returns a MyVector of associations between an address and number of students who live at that address
+  public static MyVector<Association<String, Integer>> readIn(MyVector<Student> vec){
+    MyVector<Association<String, Integer>> list = new MyVector<Association<String, Integer>>();
+    for(Student s : vec){
+      //Split address to find common addresses
+      String[] address = s.getAddress().split(" ");
+      String commonAddress = address[0];
+      Association<String, Integer> temp = new Association<String, Integer>(commonAddress, 0);
+      if(list.contains(temp)){
+        temp.setValue(list.get(list.indexOf(temp)).getValue() + 1);
+        list.setElementAt(temp, list.indexOf(temp));
+      } else {
+        temp.setValue(1);
+        list.add(temp);
+      }
+    }
+    return list;
+  }
+
   public static void main(String[] args){
+    //COPY THIS METHOD INTO ANALYZEBOOK
     MyVector<Student> vec = new MyVector<Student>();
     Scanner in = new Scanner(System.in);
     //Change to i < 2,052 when using real phonebook
-    for(int i = 0; i < 15; i++){
+    for(int i = 0; i < 2052; i++){
       String name = in.nextLine();
       String address = in.nextLine();
       long campusNum = in.nextLong();
@@ -31,5 +52,15 @@ public class AddressComparator implements Comparator<Association<String, Integer
       System.out.println(in.nextLine());
     }
     System.out.println(vec);
+    //Read in addresses and tally num addresses
+    MyVector<Association<String, Integer>> list = readIn(vec);
+    list.sort(new AddressComparator());
+    System.out.println(list);
+    //From list, address shared by most students is "Williams." Print out their names
+    for(Student s: vec){
+      if(s.getAddress().split(" ")[0].equals("Williams")){
+        System.out.println(s.getName());
+      }
+    }
   }
 }
